e3e4dcf7ded8fd96dd1c4fb4efc9d70ca8e48ca1
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-05-08 10:43:51 +0200

    save user data

--- save.php
+++ save.php
@@ -2,7 +2,12 @@
 $row = $_POST['row'];
 $records = json_decode(file_get_contents('done.json'), true);
 
-$records[$row['id']] = ['done' => $row['done'], 'observations' => $row['observations'], 'traitement' => $row['traitement'], 'datetraitement' => $row['datetraitement']];
+$records[$row['id']] = ['done' => $row['done'],
+  'observations' => $row['observations'],
+  'traitement' => $row['traitement'],
+  'datetraitement' => $row['datetraitement'],
+  'author' => $_SERVER['REMOTE_USER'],
+  'updatetime' => time()];
 
 file_put_contents('done.json', json_encode($records));
 file_put_contents('done_' . time() . '.json', json_encode($records));
